bca0f2683fb1d43c6520ed2b4152c295707f3037
Hai Nguyen Van <hai-nguyen@cybozu.vn>
2022-06-04 11:46:41 +0700

    init project

---
+++ README.md
@@ -0,0 +1,104 @@
+# Automatic GitHub Release
+
+Automatically generate a release when the package.json version changes. The release name and tag will match the new version. If no releases yet exist, this action will create the first release.
+
+The release notes will contain a change log generated from git history in the following format:
+
+```md
+- f0d91bd Making progress
+- 275e3e2 Initial commit
+```
+
+## Assumptions
+
+This action makes a few assumptions:
+
+- `actions/checkout@v2` with `fetch-depth: 0` is used before this action runs. That allows this action to have all the information it needs to generate the change log from the git history.
+- You are only releasing from one branch
+- It is only used during `push`
+
+## Usage Example
+
+```yml
+name: Release
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  build:
+    name: Release
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v2
+        with:
+          fetch-depth: 0
+      - name: Release
+        uses: justincy/github-action-npm-release@v1.2.0
+        id: release
+      - name: Print release output
+        if: ${{ steps.release.outputs.released == 'true' }}
+        run: echo Release ID ${{ steps.release.outputs.release_id }}
+```
+
+Works great in tandem with auto-publishing. Here's an example for the GitHub Package Registry:
+
+```yml
+name: Release
+on:
+  push:
+    branches:
+      - master
+
+jobs:
+  release:
+    name: Release
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout repository
+        uses: actions/checkout@v2
+        with:
+          fetch-depth: 0
+      - name: Automatic GitHub Release
+        uses: justincy/github-action-npm-release@2.0.1
+        id: release
+      - uses: actions/setup-node@v1
+        if: steps.release.outputs.released == 'true'
+        with:
+          registry-url: 'https://npm.pkg.github.com'
+      - name: Publish
+        if: steps.release.outputs.released == 'true'
+        run: npm publish
+        env:
+          NODE_AUTH_TOKEN: ${{ secrets.GITHUB_TOKEN }}
+```
+
+## Inputs
+
+- `token`: Personal access token for GitHub authentication. Optional. Defaults to `${{ github.token }}`.
+- `path`: Path of the package.json file that will be examined. Optional. Defaults to `${{ github.workspace }}`.
+
+## Outputs
+
+- `released`: Set to true when a release is created.
+- `html_url`: The URL for viewing the release in a browser.
+- `upload_url`: The URL for uploading assets to the release.
+- `release_id`: ID of the release.
+- `release_tag`: Tag of the release.
+- `release_name`: Name of the release.
+
+## Why re-invent the wheel?
+
+- [Publish to npm](https://github.com/marketplace/actions/publish-to-npm)
+- [Release Me Action](https://github.com/ridedott/release-me-action)
+    - I don't want to be forced to use any specific commit format. I want a new `version` to be the only signal to release.
+- [Version Check](https://github.com/marketplace/actions/version-check)
+    - It only checks commits in a single push. If the workflow run associated with the push that has the new version fails and you push again to fix it, then the second workflow run isn't associated with the commits where the version changed and therefore it doesn't detect that the version changed.
+- I wanted to learn more about GitHub actions.
+
+## Possible future enhancements
+
+- Add options for draft and pre-release
+- Add option for custom git-log format
---
+++ index.js
@@ -0,0 +1,119 @@
+const core = require('@actions/core');
+const {Octokit} = require('@octokit/rest');
+const {join: pathJoin} = require('path');
+const {promisify} = require('util');
+const exec = promisify(require('child_process').exec);
+
+const octokit = new Octokit({
+  auth: core.getInput('token')
+});
+
+async function main() {
+
+  core.info(`Repository ${process.env.GITHUB_REPOSITORY}`);
+  const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/');
+
+  // 1. Get the current version
+  const {version: newVersion} = __non_webpack_require__(pathJoin(core.getInput('path'), 'package.json'));
+  core.info(`New version: ${newVersion}`);
+
+  // 2. Get the latest release version and hash
+  // We'll use the version for deciding whether or not we need to create
+  // a new release (newVersion !== latestVersion) and we'll use the
+  // sha to generate a change log from that commit up until the current.
+  let beginningSha;
+  let firstRelease = false;
+  let previousVersion;
+  const {data: [latestRelease]} = await octokit.repos.listReleases({
+    owner,
+    repo,
+    per_page: 1
+  });
+
+  // Handle scenario where there is no release yet
+  if (!latestRelease) {
+    firstRelease = true;
+    core.info('Existing release. Release notes will contain all commits from the beginning of the repo.');
+  }
+
+  // There are other releases
+  else {
+    // Should I try to checkout the code and pull the version from package.json?
+    // That's a lot of work. I think it's better to assume that the tag will contain the version.
+    // Assume that this lib is creating the versions and that tags are in the format we expect.
+    previousVersion = latestRelease.tag_name;
+    core.info(`Previous release version: ${previousVersion}`);
+
+    // Ideally we could also get the sha from `target_commitish` on the release, but sometimes
+    // `target_commitish` is a branch name. That doens't work for us because the branch name
+    // would be master and calling `git log` with the range `master..HEAD` would give us nothing.
+    // To get a sha, we get the commit that the tag points to.
+    try {
+      const {data} = await octokit.repos.getCommit({
+        owner,
+        repo,
+        ref: previousVersion
+      });
+      beginningSha = data.sha;
+      core.info(`Previous release sha: ${beginningSha}`);
+    } catch (error) {
+      core.setFailed(`Unable to get the commit for tag ${previousVersion}.`);
+      core.error(error);
+      process.exit();
+    }
+  }
+
+  // 3. If versions are different or if its the first release
+  if (firstRelease || previousVersion !== newVersion) {
+    
+    // 1. Generate the change log
+    let changeLog;
+    try {
+      const {stdout, stderr} = await exec(`git log ${firstRelease ? '' : `${beginningSha}..HEAD`} --pretty=format:"- %h %s"`);
+      changeLog = stdout.trim();
+      core.info(`Change Log:\n${changeLog}`);
+    } catch (error) {
+      core.setFailed('Unable to generate the change log.');
+      core.error(error);
+      process.exit();
+    }
+
+    // 2. Publish a release
+    try {
+      const {data} = await octokit.repos.createRelease({
+        owner, 
+        repo, 
+        tag_name: newVersion, 
+        name: newVersion,
+        target_commitish: process.env.GITHUB_SHA,
+        body: changeLog,
+        
+      });
+      // What output should we provide?
+      // https://docs.github.com/en/rest/reference/repos#get-a-release
+      core.info(`Created release ${newVersion}`);
+      core.setOutput('released', true);
+      core.setOutput('html_url', data.html_url);
+      core.setOutput('upload_url', data.upload_url);
+      core.setOutput('release_id', data.id);
+      core.setOutput('release_tag', data.tag_name);
+      core.setOutput('release_name', data.name);
+      core.info(`upload_url: ${data.upload_url}`);
+    } catch (error) {
+      core.setFailed(`Failed to create release ${newVersion} for ${owner}/${repo}#${process.env.GITHUB_SHA}`);
+      core.error(error);
+      core.setOutput('released', true);
+      core.debug(JSON.stringify(error.headers));
+      core.debug(JSON.stringify(error.request));
+      process.exit();
+    }
+  }
+  
+  // Nothing to do
+  else {
+    core.info('Version has not changed.');
+  }
+}
+
+main();
+// https://github.com/msusdev/end-to-end-github-actions/blob/main/presentations/ci_github_actions.md
