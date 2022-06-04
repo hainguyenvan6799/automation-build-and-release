e3518d1e07995f78b894a72294884ee1008b074a
Hai Nguyen Van <hai-nguyen@cybozu.vn>
2022-06-04 20:13:27 +0700

    Update to version 1.0.1

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ async function main() {
       core.setOutput('release_tag', data.tag_name);
       core.setOutput('release_name', data.name);
       core.info(`upload_url: ${data.upload_url}`);
+      core.info(`release_name: ${data.name}`);
     } catch (error) {
       core.setFailed(`Failed to create release ${newVersion} for ${owner}/${repo}#${process.env.GITHUB_SHA}`);
       core.error(error);
